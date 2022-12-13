169ffa1aa9a13f781ce0d19ad790bfae59031312
Nikolay Pavlin <Afaneor@gmail.com>
2022-12-13 15:08:12 +0300

    fix: port as integer

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def main():
     uvicorn.run(
         app="server.server:app",
         host=config.APP_HOST or '0.0.0.0',
-        port=config.APP_PORT or '8000',
+        port=config.APP_PORT or 8000,
         reload=True if config.ENV != "production" else False,
         workers=1,
     )
