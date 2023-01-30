137c7a3ec90ce02b4c2fe2111d73f2f02671cfb3
Nikolay Pavlin <afaneor@gmail.com>
2023-01-30 22:57:27 +0300

    feat: added database with tortoise-orm

---
+++ app/db/startup.py
@@ -0,0 +1,12 @@
+from tortoise import Tortoise
+
+import config
+
+
+async def init():
+    # Here we connect to a SQLite DB file.
+    # also specify the app name of "models"
+    # which contain models from "app.models"
+    await Tortoise.init(config=config.DB_CONFIG)
+    # Generate the schema
+    await Tortoise.generate_schemas()
