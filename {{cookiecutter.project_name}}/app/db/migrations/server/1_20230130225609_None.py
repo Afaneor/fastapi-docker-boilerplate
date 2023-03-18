c2b6cb2040e1ff259668b21cfea682b64ad20368
Nikolay Pavlin <afaneor@gmail.com>
2023-03-18 14:40:11 +0300

    fix: reverted db module

---
+++ {{cookiecutter.project_name}}/app/db/migrations/server/1_20230130225609_None.py
@@ -0,0 +1,16 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
