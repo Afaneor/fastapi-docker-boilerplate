cc236b464eada9896ebbeb4b50cb0a4f4c051f8a
npavlin <np@letsnova.ru>
2024-10-13 11:17:03 +0300

    fix: production_domain settings

--- {{cookiecutter.project_name}}/app/config/envs/production.py
+++ {{cookiecutter.project_name}}/app/config/envs/production.py
@@ -9,7 +9,7 @@ class ProductionConfig(BaseSettings):
     workers: int = 8
 
     # CORS settings
-    cors_origins: list[str] = ["{{production_domain}}"]
+    cors_origins: list[str] = ["{{cookiecutter.production_domain}}"]
     cors_allow_credentials: bool = True
     cors_allow_methods: list[str] = ["GET", "POST", "PUT", "DELETE"]
     cors_allow_headers: list[str] = ["*"]
