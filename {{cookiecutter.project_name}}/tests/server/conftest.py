45b7d7ec0b73af84e45249e59217284ece9098f6
npavlin <np@letsnova.ru>
2024-10-20 13:07:03 +0300

    fix: test client fixture

--- {{cookiecutter.project_name}}/tests/server/conftest.py
+++ {{cookiecutter.project_name}}/tests/server/conftest.py
@@ -1,13 +1,27 @@
 import pytest
-from starlette.testclient import TestClient
+from asgi_lifespan import LifespanManager
+from httpx import AsyncClient, ASGITransport
+from tortoise import Tortoise
 
 from app.server.server import app
 
 
-@pytest.fixture(scope='session', autouse=True)
-def client() -> TestClient:
+@pytest.fixture(scope='module', autouse=True)
+async def client():
+    async with LifespanManager(app) as manager:
+        async with AsyncClient(
+            transport=ASGITransport(app=app),
+            base_url="http://test",
+        ) as client:
+            yield client
+
+
+
+@pytest.fixture(autouse=True)
+async def clean_db():
     """
-    Фикстура для тестирования API.
+    Фикстура для очистки базы данных перед каждым тестом, чтобы тесты были независимыми.
     """
-    with TestClient(app) as c:
-        yield c
+    for model in Tortoise.apps.get("models", {}).values():
+        await model.all().delete()
+
